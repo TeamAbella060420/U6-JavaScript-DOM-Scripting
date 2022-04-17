a75b6139680adbb5216bbdfad382de2b05f53d3d
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-17 14:39:00 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -4,7 +4,9 @@ const ul = document.getElementById('invitedList');
 
 function createLI(text) {
         const li = document.createElement('li');
-        li.textContent = text;
+        const span = document.createElement('span');
+        span.textContent = text;
+        li.appendChild(span);
         const label = document.createElement('label');
         label.textContent = 'Confirmed';
         const checkbox = document.createElement('input');
@@ -50,6 +52,13 @@ ul.addEventListener('click', (e) => {
         if (button.tagName === 'remove'){
         ul.removeChild(li);
     } else if (button.tagName === 'edit');
+        const span = li.firstElementChild;
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = span.textContent;
+        li.insertBefore(input, span);
+        li.removeChild(span);
+        button.textContent = 'save';
     }
 });
 
